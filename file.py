ffe600548c7a30c456b91997d819826ad0ae8185
YamagaShiny <adrien.perrin01@gmail.com>
2025-12-02 18:58:39 +0100

    Adding file.py and finishing fonction consommateur
    - Replaced the list-based command queue with a FileCommandes class
    - Updated consommateur logic to use the new queue
    - Added file.py to implement the FileCommandes class.

---
+++ file.py
@@ -0,0 +1,27 @@
+class FileCommandes:
+    def __init__(self):
+        self.commandes = [] # On utilise une liste comme conteneur interne
+
+    def enfiler(self, commande):
+        """Ajoute une commande à la fin de la file (Action Acheteur)"""
+        self.commandes.append(commande)
+    
+    def defiler(self):
+        """Retire et renvoie la commande du début de la file (Action Restaurateur)"""
+        if not self.est_vide():
+            # pop(0) retire l'élément à l'index 0 (le plus vieux)
+            return self.commandes.pop(0) 
+        return None
+
+    def est_vide(self):
+        """Vérifie si la file est vide"""
+        return len(self.commandes) == 0
+
+    def voir_prochaine(self):
+        """Regarde la prochaine commande sans la retirer"""
+        if not self.est_vide():
+            return self.commandes[0]
+        return None
+        
+    def tout_afficher(self):
+        return self.commandes
